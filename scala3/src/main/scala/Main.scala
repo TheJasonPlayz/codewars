a5c11486dbe98017301e3df6e18e84257a0bd4fe
TheJasonPlayz <thejasonplayz@gmail.com>
2023-05-10 16:24:54 -0600

    Even Or Odd func and tests

--- scala3/src/main/scala/Main.scala
+++ scala3/src/main/scala/Main.scala
@@ -1,1 +1,6 @@
-
+package com.solutions
+object Solutions {
+  def evenOrOdd(number: Int): String = {
+    if (number % 2 == 0) "Even" else "Odd"
+  }
+}
--- scala3/src/test/scala/MySuite.scala
+++ scala3/src/test/scala/MySuite.scala
@@ -1,4 +1,10 @@
 import org.scalatest.flatspec.AnyFlatSpec
+import com.solutions.Solutions
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
-class FlatSpec extends AnyFlatSpec {}
+class Tests extends AnyFlatSpec {
+  "1 and 2" should "return Odd then Even" in {
+    val (odd, even) = (Solutions.evenOrOdd(1), Solutions.evenOrOdd(2))
+    assert(odd == "Odd" && even == "Even")
+  }
+}
