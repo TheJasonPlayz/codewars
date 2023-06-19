239b6b45db97f6cbde063f94c0d76b429fec7ba7
TheJasonPlayz <thejasonplayz@gmail.com>
2023-06-19 12:17:47 -0600

    Removed uneeded comment

--- scala3/src/test/scala/Tests.scala
+++ scala3/src/test/scala/Tests.scala
@@ -1,7 +1,6 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import com.solutions.Solutions
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
+
 class Tests extends AnyFlatSpec {
   "1 and 2" should "return Odd then Even" in {
     val (odd, even) = (Solutions.evenOrOdd(1), Solutions.evenOrOdd(2))
